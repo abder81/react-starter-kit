6d2f47704cfd7a113c9dc9f88c8fad2a4161784d
abder81 <benabderrahman59@gmail.com>
2025-05-14 17:05:07 +0100

    first commit

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,22 @@ Route::get('/', function () {
 })->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
+
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+
+    Route::get('utilisateurs', function () {
+        return Inertia::render('users');
+    })->name('utilisateurs');
+
+    Route::get('roles', function () {
+        return Inertia::render('roles');
+    })->name('roles');
+
+    Route::get('documents', function () {
+        return Inertia::render('docs');
+    })->name('documents');
 });
 
 require __DIR__.'/settings.php';
